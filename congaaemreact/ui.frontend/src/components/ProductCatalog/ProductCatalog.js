a7dffca3fc785c33bd29d3015d2f60915424bc50
aranja20 <aranja20@inlen8520025075.corp.capgemini.com>
2022-08-19 21:31:24 +0530

    committing changes for: 1.productcatalog-dialog 2.category Api js. 3.catprod js 4.Import js 5.List_category 6.product catalog css and js

---
+++ congaaemreact/ui.frontend/src/components/ProductCatalog/ProductCatalog.js
@@ -0,0 +1,25 @@
+import React, {Component} from 'react';
+import {MapTo} from '@adobe/aem-react-editable-components';
+import CatProd from '../Category_Api/CatProd';
+
+export const ProductCatalogEditConfig = {
+
+    emptyLabel: 'Product Catalog',
+
+    isEmpty: function(props) {
+        return !props || !props.message || props.message.trim().length < 1;
+    }
+};
+
+export default class ProductCatalog extends Component {
+
+  render() {
+    return (
+           <div>
+           <CatProd/>
+           </div>
+    );
+  }
+}
+
+MapTo('congaaemreact/components/productcatalog')(ProductCatalog, ProductCatalogEditConfig);
